c4c4281c4934631b2220fdf8cb52a4ce99daf1b9
José Manuel Marrón <manemarron@gmail.com>
2017-03-22 00:36:08 -0400

    Test getToken amex

--- test/gateway/amex/index.js
+++ test/gateway/amex/index.js
@@ -1,9 +1,11 @@
 var createTokenSuite = require('./createToken');
 var deleteTokenSuite = require('./deleteToken');
+var getTokenSuite = require('./getToken');
 var getGatewayInfoSuite = require('./getGatewayInfo');
 
 module.exports = function describeAmex() {
 	describe('getGatewayInfo', getGatewayInfoSuite);
 	describe('createToken', createTokenSuite);
 	describe('deleteToken', deleteTokenSuite);
+	describe('getToken', getTokenSuite);
 };
