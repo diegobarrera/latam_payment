d3405867c5b0132e73c2f97b3909e49d05ee4f99
José Manuel Marrón <manemarron@gmail.com>
2017-03-22 00:28:01 -0400

    Test deleteToken amex

---
+++ test/gateway/amex/deleteToken.js
@@ -0,0 +1,43 @@
+var expect = require('chai').expect;
+var amex = require('gateway/amex');
+
+var credentials = {
+	merchantId: 'TEST9353874515',
+	password: 'd0f2cd569d431efcf62f5409cea56fd7',
+};
+
+var token;
+module.exports = function deleteTokenSuite() {
+	beforeEach(function beforEach(done) {
+		var payload = {
+			sourceOfFunds: {
+				provided: {
+					card: {
+						expiry: {
+							month: '05',
+							year: '17',
+						},
+						number: '345678000000007',
+						securityCode: '0000'
+					},
+				},
+				type: 'CARD',
+			},
+		};
+		amex.createToken(payload, credentials, function(err, body) {
+			token = body.token;
+			done();
+		});
+	});
+	afterEach(function afterEach(done) {
+		amex.deleteToken(token, credentials, function() {
+			done();
+		});
+	});
+	it('Delete card token', function test(done) {
+		amex.deleteToken(token, credentials, function(err, body) {
+			expect(err).to.not.exist;
+			done();
+		});
+	});
+};
--- test/gateway/amex/index.js
+++ test/gateway/amex/index.js
@@ -1,7 +1,9 @@
 var createTokenSuite = require('./createToken');
+var deleteTokenSuite = require('./deleteToken');
 var getGatewayInfoSuite = require('./getGatewayInfo');
 
 module.exports = function describeAmex() {
 	describe('getGatewayInfo', getGatewayInfoSuite);
 	describe('createToken', createTokenSuite);
+	describe('deleteToken', deleteTokenSuite);
 };
